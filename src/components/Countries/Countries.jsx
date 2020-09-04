b3fe3d330aeaf75669fab9b2a43cd306ca6fe4ec
RobertoCastelli <roberto.castelli.teal@gmail.com>
2020-09-04 17:28:59 +0200

    countries list added

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { fetchData } from "../fetchData";
 
 const Countries = () => {
+  //--> State
+  const [fetchedCountries, setFetchedCountries] = useState(null);
+
+  //--> URLs
+  const url = "https://covid19.mathdro.id/api/countries";
+
+  //--> Fetch countries data
+  useEffect(() => {
+    const fetchDataTemp = async () => {
+      //--> Deconstructoring data in fetch function
+      const { countries } = await fetchData(url);
+      setFetchedCountries(countries);
+    };
+    fetchDataTemp();
+  }, []);
+
+  //--> Show empty string while fetching data
+  if (fetchedCountries === null) {
+    return <div></div>;
+  }
+
+  //--> Render
   return (
     <div>
-      <p>Countries</p>
+      <select>
+        <option value="global">Global</option>
+        {fetchedCountries.map((country) => {
+          return <option key={country.name}>{country.name}</option>;
+        })}
+      </select>
     </div>
   );
 };
